cf51fc5f9e5101d8d5a7976b5f93e0623c022ab7
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-04-19 18:17:24 +0200

    fix: showmobile menu exiting when click inside menu

--- src/modules/showMobileMenu.js
+++ src/modules/showMobileMenu.js
@@ -1,18 +1,21 @@
 const showMobileMenu = (button, menu) => {
   const toggleMenu = () => menu.classList.toggle("show-mobile-menu");
-  const toggleShowClass = () => {
-    if (menu.classList.contains("show-mobile-menu")) {
-      menu.classList.remove("show-mobile-menu");
-    }
-  };
-
   button.addEventListener("click", event => {
     event.stopPropagation();
     toggleMenu();
   });
-  document.documentElement.addEventListener("click", () => {
-    toggleShowClass();
-  });
+
+  const closeMobileMenu = event => {
+    if (
+      !menu.contains(event.target) &&
+      menu.classList.contains("show-mobile-menu")
+    ) {
+      menu.classList.remove("show-mobile-menu");
+    }
+  };
+
+  // Attach this handler correctly with the event passed to the closeMobileMenu function.
+  document.documentElement.addEventListener("click", closeMobileMenu);
 };
 
 export default showMobileMenu;
